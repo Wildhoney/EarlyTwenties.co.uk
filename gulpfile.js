bbeea67487b2a2859638d5c135d1f31a78b0ee50
Wildhoney <adam.timberlake@gmail.com>
2015-02-27 10:49:03 +0000

    Added fonts in deployment process

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,23 @@
 
     var JS_FILES      = ['public/js/App.js', 'public/js/**/*.js'],
         SASS_FILES    = ['public/scss/default.scss', 'public/scss/**/*.scss'],
-        LIBRARY_FILES = ['public/vendor/angular/angular.js', 'public/vendor/mousetrap/mousetrap.js'];
+        JS_LIBRARIES  = ['public/vendor/angular/angular.js', 'public/vendor/mousetrap/mousetrap.js'],
+        CSS_LIBRARIES = ['public/vendor/font-awesome/css/font-awesome.css'];
 
     var gulp         = require('gulp'),
         uglify       = require('gulp-uglify'),
         sass         = require('gulp-sass'),
         rename       = require('gulp-rename'),
         concat       = require('gulp-concat'),
+        flatten      = require('gulp-flatten'),
+        cssmin       = require('gulp-cssmin'),
         jshint       = require('gulp-jshint'),
         autoprefixer = require('gulp-autoprefixer'),
         processhtml  = require('gulp-processhtml');
 
     gulp.task('js', function() {
 
-        return gulp.src([].concat(LIBRARY_FILES, JS_FILES))
+        return gulp.src([].concat(JS_LIBRARIES, JS_FILES))
                    .pipe(uglify())
                    .pipe(concat('all.js'))
                    .pipe(rename('earlytwenties.js'))
@@ -35,6 +38,24 @@
 
         return gulp.src(SASS_FILES[0])
                    .pipe(sass())
+                   .pipe(rename('earlytwenties.css'))
+                   .pipe(gulp.dest('public/build'));
+
+    });
+
+    gulp.task('fonts', function() {
+
+        return gulp.src('public/vendor/font-awesome/fonts/*', { base: '.' })
+                   .pipe(flatten())
+                   .pipe(gulp.dest('public/fonts'));
+
+    });
+
+    gulp.task('css', ['sass'], function() {
+
+        return gulp.src(CSS_LIBRARIES.concat('public/build/earlytwenties.css'))
+                   .pipe(concat('all.css'))
+                   .pipe(cssmin())
                    .pipe(autoprefixer({ browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'] }))
                    .pipe(rename('earlytwenties.css'))
                    .pipe(gulp.dest('public/build'));
@@ -50,9 +71,8 @@
     });
 
     gulp.task('test', ['hint']);
-
-    gulp.task('build-dev', ['js', 'sass']);
-    gulp.task('build-prod', ['html', 'js', 'sass']);
+    gulp.task('build-dev', ['js', 'sass', 'css', 'fonts']);
+    gulp.task('build-prod', ['html', 'js', 'sass', 'css', 'fonts']);
     gulp.task('default', ['test', 'build-dev']);
 
     gulp.task('watch', function() {
