3db0d65d76e32e5b9c919125d82ba271128aae35
Wildhoney <adam.timberlake@gmail.com>
2015-12-03 22:38:40 +0000

    Added basic animation for transition

--- src/js/core.js
+++ src/js/core.js
@@ -1,6 +1,15 @@
 import {fromEvent} from 'baconjs';
 
 fromEvent(document, 'mousewheel')
-    .filter(event => Math.abs(event.deltaY) > 100)
+    .filter(event => Math.abs(event.deltaY) > 50)
     .debounceImmediate(250)
-    .onValue(event => console.log('Move'));
+    .onValue(event => {
+
+        const element = document.querySelector('section.background');
+        const current = element.querySelector('div:nth-of-type(1)');
+        const next    = element.querySelector('div:nth-of-type(2)');
+
+        current.classList.add('shift');
+        next.classList.add('shift');
+
+    });
