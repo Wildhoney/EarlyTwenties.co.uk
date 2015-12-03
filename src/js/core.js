15227d2a8d7d082d28a4cee6f209cb5f6d0a11e9
Wildhoney <adam.timberlake@gmail.com>
2015-12-03 21:19:28 +0000

    Added initial logic for scrolling

--- src/js/core.js
+++ src/js/core.js
@@ -1,1 +1,6 @@
-console.log('Maria...');
+import {fromEvent} from 'baconjs';
+
+fromEvent(document, 'mousewheel')
+    .filter(event => Math.abs(event.deltaY) > 100)
+    .debounceImmediate(250)
+    .onValue(event => console.log('Move'));
