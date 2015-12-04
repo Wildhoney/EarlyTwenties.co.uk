1240456718fbe80a3cf9a75a87d66fb954f069e0
Wildhoney <adam.timberlake@gmail.com>
2015-12-04 14:18:33 +0000

    Passing vector of images

--- src/js/core.js
+++ src/js/core.js
@@ -6,9 +6,7 @@ import {fromEvent} from 'baconjs';
 class Background extends React.Component {
 
     static propTypes = {
-        previous: React.PropTypes.string.isRequired,
-        current: React.PropTypes.string.isRequired,
-        next: React.PropTypes.string.isRequired
+        images: React.PropTypes.array.isRequired
     };
 
     /**
@@ -18,9 +16,8 @@ class Background extends React.Component {
     componentWillMount() {
 
         this.state = {
-            previous: this.props.previous,
-            current: this.props.current,
-            next: this.props.next,
+            images: this.props.images,
+            index: 0,
             animating: false
         };
 
@@ -32,10 +29,11 @@ class Background extends React.Component {
      */
     componentDidMount() {
 
-        const element  = findDOMNode(this.refs.animation);
-        const previous = findDOMNode(this.refs.previous);
-        const current  = findDOMNode(this.refs.current);
-        const next     = findDOMNode(this.refs.next);
+        const container = findDOMNode(this.refs.container);
+        const ul        = findDOMNode(this.refs.animation);
+        const previous  = findDOMNode(this.refs.previous);
+        const current   = findDOMNode(this.refs.current);
+        const next      = findDOMNode(this.refs.next);
 
         fromEvent(document, 'mousewheel')
             .filter(event => Math.abs(event.deltaY) > 100)
@@ -51,19 +49,26 @@ class Background extends React.Component {
                 const opacity = 0.7;
 
                 const swapImages = () => {
+
+                    const {index, images} = this.state;
+                    const replaceIndex    = clockwise ? index + 1 : index - 1;
+
                     this.setState({
-                        current: this.state.next,
+                        index: images[replaceIndex] ? replaceIndex : (clockwise ? 0 : images.length - 1),
                         animating: false
                     });
+
                 };
 
                 timeline
-                    .to(element, duration, { top: clockwise ? '-100%' : '100%' }, 'main')
+                    .to(container, duration, { transform: 'scale(1)' }, 'main')
+                    .to(ul, duration, { top: clockwise ? '-100%' : '100%' }, 'main')
                     .to(current, duration, { opacity: 0, transform: 'scale(0.7)' }, 'main')
                     .to(clockwise ? next : previous, duration, { opacity, transform: 'scale(1)' }, 'main')
-                    .to(element, 0, { top: '0%' }, 'reset')
+                    .to(ul, 0, { top: '0%' }, 'reset')
                     .to(current, 0, { opacity, transform: 'scale(1)' }, 'reset')
-                    .to(clockwise ? next : previous, 0, { opacity, transform: 'scale(0.7)', onComplete: swapImages }, 'reset');
+                    .to(clockwise ? next : previous, 0, { opacity, transform: 'scale(0.7)', onComplete: swapImages }, 'reset')
+                    .to(container, 3, { transform: 'scale(1.02)' });
 
             });
 
@@ -75,14 +80,16 @@ class Background extends React.Component {
      */
     render() {
 
-        const {previous, current, next} = this.state;
+        const {images, index} = this.state;
+        const previous = images[index - 1] || images[images.length - 1];
+        const next = images[index + 1] || images[0];
 
         return (
-            <section className="background">
+            <section className="background" ref="container">
                 <ul className="inner" ref="animation">
-                    <li ref="previous" style={{ backgroundImage: `url(${previous})` }} />
-                    <li ref="current" style={{ backgroundImage: `url(${current})` }} />
-                    <li ref="next" style={{ backgroundImage: `url(${next})` }} />
+                    <li ref="previous"><img src={`images/${previous}`} /></li>
+                    <li ref="current"><img src={`images/${images[index]}`} /></li>
+                    <li ref="next"><img src={`images/${next}`} /></li>
                 </ul>
             </section>
         );
@@ -94,9 +101,12 @@ class Background extends React.Component {
 document.addEventListener('DOMContentLoaded', () => {
 
     render(
-        <Background previous="images/landscape-large-03.jpg"
-                    current="images/Inside_the_Batad_rice_terraces.jpg"
-                    next="images/landscape-large-03.jpg" />,
+        <Background images={[
+            '1453387_10153573511785500_1899755281_n.jpg',
+            '1501290_10153594457450500_512797407_o.jpg',
+            '10919772_10204037928682634_3559155281616018900_o.jpg',
+            '11537596_10205200764592805_3267665941076830075_o.jpg'
+        ]} />,
         document.body
     );
 
